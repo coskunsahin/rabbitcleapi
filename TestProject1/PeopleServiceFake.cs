07cf1ffd1f9cda55daa4c975079d160167a8a1d3
COSKUN <COSKUN@DESKTOP-D3M1FK4>
2023-01-05 03:29:27 +0300

    SAHTA DATA GUNCELEEME

--- TestProject1/PeopleServiceFake.cs
+++ TestProject1/PeopleServiceFake.cs
@@ -14,25 +14,25 @@ namespace TestServices
     {
         private readonly List<People> _people;
 
-        public ShoppingCartServiceFake()
-        {
-            _shoppingCart = new List<ShoppingItem>()
-            {
-                new ShoppingItem() { Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
-                    Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
-                new ShoppingItem() { Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
-                    Name = "Diary Milk", Manufacturer="Mad Cow", Price = 4.00M },
-                new ShoppingItem() { Id = new Guid("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
-                    Name = "Frozen Pizza", Manufacturer="Uncle Mickey's", Price = 12.00M }
-            };
-        }
+        //public ShoppingCartServiceFake()
+        //{
+        //    _shoppingCart = new List<ShoppingItem>()
+        //    {
+        //        new ShoppingItem() { Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+        //            Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
+        //        new ShoppingItem() { Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
+        //            Name = "Diary Milk", Manufacturer="Mad Cow", Price = 4.00M },
+        //        new ShoppingItem() { Id = new Guid("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
+        //            Name = "Frozen Pizza", Manufacturer="Uncle Mickey's", Price = 12.00M }
+        //    };
+        //}
 
-        public IEnumerable<ShoppingItem> GetAllItems()
-        {
-            return _shoppingCart;
-        }
+        //public IEnumerable<ShoppingItem> GetAllItems()
+        //{
+        //    return _shoppingCart;
+        //}
 
-        public ShoppingItem Add(ShoppingItem newItem)
+        //public ShoppingItem Add(ShoppingItem newItem)
         {
             newItem.Id = Guid.NewGuid();
             _shoppingCart.Add(newItem);
