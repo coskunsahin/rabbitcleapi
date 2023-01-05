854fcb1b2938e21a10a89924b2935a791a1580be
COSKUN <COSKUN@DESKTOP-D3M1FK4>
2023-01-05 03:11:57 +0300

    interface olusturuldu

--- TestProject1/IPeopleServiceFAKE.cs
+++ TestProject1/IPeopleServiceFAKE.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using fakestoreapi;
+using fakestoreapi.domain.Entities;
+
 namespace TestServices
 {    
     
     
     public interface IPeopleServiceFAKE
     {
-        
+        IEnumerable<People> GetAllIPeople();
+        People Add(People newPeople);
+        People GetById(People PeopleID);
+        void Remove(People peopleID);
     }
 }
--- TestProject1/PeopleServiceFake.cs
+++ TestProject1/PeopleServiceFake.cs
@@ -1,6 +1,6 @@
 ﻿
 using fakestoreapi.application.Queries;
-using fakestoreapi.domain.Entities.Domain.Entities;
+using fakestoreapi.domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
