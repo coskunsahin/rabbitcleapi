a4de484a4fb0d1cea8332947ee4d1d08fc89f1ca
COSKUN <COSKUN@DESKTOP-D3M1FK4>
2023-01-05 03:01:39 +0300

    model class  eklendi

--- TestProject1/People.cs
+++ TestProject1/People.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using fakestoreapi.domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using fakestoreapi;
 namespace TestServices
 {
-    internal interface People
+    public class People
     {
+        public int PeopleID { get; set; }
+        public string? Name { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Company { get; set; }
+
+
+        public DateTime ReportTime { get; set; }
+        public List<Contact>? Contacts { get; set; }
+
     }
 }
